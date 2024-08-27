9f77fd39fc06682d910e5e74e9c5ef15cc3c8468
Roman Matsypula <r.matsypula@letyshops.com>
2024-08-28 01:07:00 +0300

    add button component with <ng-content> like slot in vue.js

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../shared/button/button.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
 })
 export class HeaderComponent {}
